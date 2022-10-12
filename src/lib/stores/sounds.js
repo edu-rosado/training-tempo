6b310c3c23ad9a60ef0cc455eef531c3860e4caa
edu-rosado <edurosgam@gmail.com>
2022-10-12 19:41:37 +0200

    New spanish set 5 by 5 and default set is also 5 by 5 from 30 onwards

--- src/lib/stores/sounds.js
+++ src/lib/stores/sounds.js
@@ -30,52 +30,57 @@ function setupSoundStore_aux() {
         tempVal = JSON.parse(localStorage.sounds);
     } catch {}
     if (typeof tempVal === "object" && tempVal !== null) {
-        let found = false
-        for (let i = 0; i < tempVal.soundSets; i++){
-            if (tempVal.soundSets[i].name === "Default spanish"){
-                found = true
-                break
-            }
-        }
-        if (!found){
-            // meter default spanish
-            const spanishSet = {
-                name: "Default spanish",
-                soundItems: [
-                    {
-                        id: "Default",
-                        sound_type: "local_file",
-                        title: "Built-in - Chimes",
-                        src: "/house-kick-bassy-punchy-4.wav",
-                    },
-                ],
-            }
-            for (let i = 1; i <= 100; i++){
-                if (i > 30 && i % 10 != 0){
-                    spanishSet.soundItems.push({
-                        id: i.toString(),
-                        sound_type: "undefined",
-                        title: "Not set",
-                    })
-                }
-                else{
-                    spanishSet.soundItems.push({
-                        id: i.toString(),
-                        sound_type: "local_file",
-                        title: "Local file - " + i,
-                        src: "/spanish_counts/" + i + ".mp3",
-                    })
-                }
+        setupDefaultSpanish(tempVal, 1, "Default spanish");
+        setupDefaultSpanish(tempVal, 5, "Default spanish 5 by 5");
 
-            }
-            tempVal.soundSets.push(spanishSet)
-        }
-        
         soundStore.set(tempVal);
     }
     soundStore.subscribe((newVal) => {
         localStorage.sounds = JSON.stringify(newVal);
     });
+}
+
+function setupDefaultSpanish(tempVal, increment, title) {
+    // Increment: solo ara valores < 30, cual es el incremento entre ellos
+    let found = false;
+    for (let i = 0; i < tempVal.soundSets; i++) {
+        if (tempVal.soundSets[i].name === title) {
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        // meter default spanish
+        const spanishSet = {
+            name: title,
+            soundItems: [
+                {
+                    id: "Default",
+                    sound_type: "local_file",
+                    title: "Built-in - Chimes",
+                    src: "/house-kick-bassy-punchy-4.wav",
+                },
+            ],
+        };
+        for (let i = 1; i <= 100; i++) {
+            if ((i > 30 && i % 5 != 0) || (i % increment != 0 && i != 1)) {
+                spanishSet.soundItems.push({
+                    id: i.toString(),
+                    sound_type: "undefined",
+                    title: "Not set",
+                });
+            } else {
+                spanishSet.soundItems.push({
+                    id: i.toString(),
+                    sound_type: "local_file",
+                    title: "Local file - " + i,
+                    src: "/spanish_counts/" + i + ".mp3",
+                });
+            }
+        }
+        tempVal.soundSets.push(spanishSet);
+    }
 }
 
 export function addNewSoundSet() {
