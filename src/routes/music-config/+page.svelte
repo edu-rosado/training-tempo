e6a168331596946c783d62a4c2f7ae290215f355
edu-rosado <edurosgam@gmail.com>
2022-10-18 21:10:59 +0200

    cursor pointer and code format

--- src/routes/music-config/+page.svelte
+++ src/routes/music-config/+page.svelte
@@ -1,15 +1,13 @@
 <script>
-    import MeasureTab from "./MeasureTab.svelte"
-    import ExtraSoundsTab from "./ExtraSoundsTab.svelte"
+    import MeasureTab from "./MeasureTab.svelte";
+    import ExtraSoundsTab from "./ExtraSoundsTab.svelte";
 
     let selected_tab = "measure";
-
-
 </script>
 
 <div class="grid grid-cols-2">
     <div
-        class={"p-3 border roundex-sm border-indigo-400/25 bg-indigo-100/50 text-indigo-700 font-light flex justify-center items-center" +
+        class={"p-3 border roundex-sm border-indigo-400/25 bg-indigo-100/50 text-indigo-700 font-light flex justify-center items-center cursor-pointer" +
             (selected_tab === "measure" ? " border-b-4 border-b-indigo-500 font-normal" : "")}
         on:click={() => {
             selected_tab = "measure";
@@ -18,7 +16,7 @@
         Measure sound
     </div>
     <div
-        class={"p-3 border roundex-sm border-indigo-400/25 bg-indigo-100/50 text-indigo-700 font-light flex justify-center items-center" +
+        class={"p-3 border roundex-sm border-indigo-400/25 bg-indigo-100/50 text-indigo-700 font-light flex justify-center items-center cursor-pointer" +
             (selected_tab === "extra" ? " border-b-4 border-b-indigo-500 font-normal" : "")}
         on:click={() => {
             selected_tab = "extra";
@@ -29,7 +27,7 @@
 </div>
 
 {#if selected_tab === "measure"}
-    <MeasureTab/>
+    <MeasureTab />
 {:else if selected_tab === "extra"}
-    <ExtraSoundsTab/>
+    <ExtraSoundsTab />
 {/if}
